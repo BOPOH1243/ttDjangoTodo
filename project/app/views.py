b944da85dbdfb4bdba5a61c2f3fb7764f7a38f9e
BOPOH1243 <tenpenni@mail.ru>
2025-05-18 18:18:34 +0500

    сделал фильтры на апишку

--- project/app/views.py
+++ project/app/views.py
@@ -2,14 +2,17 @@
 from rest_framework import viewsets, permissions
 from .models import Category, Task
 from .serializers import CategorySerializer, TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     #permission_classes = [permissions.IsAuthenticated]
 
-
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    #permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['telegram_user_id']
+
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app',
     'rest_framework',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
